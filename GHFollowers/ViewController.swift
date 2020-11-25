7f46ca68608eed74ebfaaa27863af330fe0ad93d
Vanessa Flores <floresv26@gmail.com>
2020-11-25 08:04:08 -0600

    Project setup

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Vanessa Flores on 11/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
