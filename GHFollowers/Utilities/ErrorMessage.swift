f55c804ad0b6d7fc17dff4701b279375431d2e08
Vanessa Flores <floresv26@gmail.com>
2020-11-26 17:18:43 -0600

    Refactor network error messages

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Vanessa Flores on 11/26/20.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername    = "This username created an invalid request. Please try again."
+    case unableToComplete   = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse    = "Invalid response from the server. Please try again."
+    case invalidData        = "The data received from the server was invalid. Please try again."
+}
