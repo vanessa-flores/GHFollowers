1b684f95ad15b911bf2a39b1283b54f612e63b06
Vanessa Flores <floresv26@gmail.com>
2020-11-27 09:33:18 -0600

    Create FollowerCell for collection view

---
+++ GHFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,52 @@
+//
+//  FollowerCell.swift
+//  GHFollowers
+//
+//  Created by Vanessa Flores on 11/27/20.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    static let reuseID = "FollowerCell"
+    let padding: CGFloat = 8
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.username
+    }
+    
+    // MARK: - Configuration
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
