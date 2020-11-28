2fc39dbd9a06486f4de81ba396bc7a64f150d9b4
Vanessa Flores <floresv26@gmail.com>
2020-11-28 14:29:35 -0600

    Create GFUserInfoHeaderViewController

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Vanessa Flores on 11/28/20.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
