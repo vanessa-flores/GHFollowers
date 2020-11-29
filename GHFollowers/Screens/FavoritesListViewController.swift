eb6192b2f32d6b4bd68b658bf4cd40f0cc975093
Vanessa Flores <floresv26@gmail.com>
2020-11-29 11:36:52 -0600

    Remove Favorite from FavoritesList

--- GHFollowers/Screens/FavoritesListViewController.swift
+++ GHFollowers/Screens/FavoritesListViewController.swift
@@ -96,4 +96,17 @@ extension FavoritesListViewController: UITableViewDataSource, UITableViewDelegat
         navigationController?.pushViewController(destinationViewController, animated: true)
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            guard let error = error else { return }
+            self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
+        }
+    }
 }
