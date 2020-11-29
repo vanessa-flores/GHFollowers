20e9c67e952f222a595ab38c307185ee9996043d
Vanessa Flores <floresv26@gmail.com>
2020-11-29 10:36:43 -0600

    Add functionality for saving user in favorites

--- GHFollowers/Screens/FollowersListViewController.swift
+++ GHFollowers/Screens/FollowersListViewController.swift
@@ -105,7 +105,35 @@ class FollowersListViewController: UIViewController {
     }
     
     @objc func addButtonTapped() {
-        print("Add button tapped")
+        showLoadingView()
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            self.dismissLoadingView()
+            
+            switch result {
+            case .success(let user):
+                let favorite = Follower(username: user.username, avatarURL: user.avatarUrl)
+                self.persistFavorite(favorite)
+                   
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+    
+    private func persistFavorite(_ favorite: Follower) {
+        PersistenceManager.updateWith(favorite: favorite, actionType: .add) { [weak self] error in
+            guard let self = self else { return }
+            
+            guard let error = error else {
+                self.presentGFAlertOnMainThread(title: "Success!", message: "You have successfully favorited this user 🎉.", buttonTitle: "Hooray!")
+                
+                return
+            }
+            
+            self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+        }
     }
     
     private func configureCollectionView() {
