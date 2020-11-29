4f665d9189810c14d73ea2f4cf2c1bb81fd792fd
Vanessa Flores <floresv26@gmail.com>
2020-11-29 11:27:45 -0600

    Display list of favorites in FavoritesViewController or EmptyStateView

--- GHFollowers/Screens/FavoritesListViewController.swift
+++ GHFollowers/Screens/FavoritesListViewController.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class FavoritesListViewController: UIViewController {
+    
+    let tableView = UITableView()
+    var favorites: [Follower] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureViewController()
+        configureTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         getFavorites()
-        
     }
     
     private func configureViewController() {
@@ -23,17 +30,62 @@ class FavoritesListViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
+    // MARK: - Configure UI
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseID)
+    }
+    
     // MARK: - Network Call
     
     private func getFavorites() {
-        PersistenceManager.retrieveFavorites { result in
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self = self else { return }
+            
             switch result {
             case .success(let favorites):
-                print(favorites)
+                if favorites.isEmpty {
+                    self.showEmptyStateView(with: "No Favorites?\nAdd one on the Follower screen", in: self.view)
+                } else {
+                    self.favorites = favorites
+                    self.displayFavorites()
+                }
+                
             case .failure(let error):
-                break
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
         }
     }
+    
+    private func displayFavorites() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.view.bringSubviewToFront(self.tableView)
+        }
+    }
 
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+
+extension FavoritesListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseID) as! FavoriteCell
+        cell.set(favorite: favorites[indexPath.row])
+        
+        return cell
+    }
+    
+    
 }
