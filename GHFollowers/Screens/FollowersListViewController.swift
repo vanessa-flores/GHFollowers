c307dbd8e7788ba6b6d3b0bb398f7a76b7b7c9d6
Vanessa Flores <floresv26@gmail.com>
2020-11-29 16:56:42 -0600

    Fix scroll to top in FollowersList when displaying a new user's followers from UserInfo screen

--- GHFollowers/Screens/FollowersListViewController.swift
+++ GHFollowers/Screens/FollowersListViewController.swift
@@ -228,7 +228,7 @@ extension FollowersListViewController: FollowersListViewControllerDelegate {
         page = 1
         followers.removeAll()
         filteredFollowers.removeAll()
-        collectionView.setContentOffset(.zero, animated: true)
+        collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: true)
         getFollowers(username: username, page: page)
     }
     
