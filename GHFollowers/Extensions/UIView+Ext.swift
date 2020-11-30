f281243390b2800738da43748387669e9a4b9313
Vanessa Flores <floresv26@gmail.com>
2020-11-30 10:18:08 -0600

    Add scrollView to UserInfoViewController to accommodate iPhone SE & 8

--- GHFollowers/Extensions/UIView+Ext.swift
+++ GHFollowers/Extensions/UIView+Ext.swift
@@ -12,4 +12,15 @@ extension UIView {
     func addSubviews(_ views: UIView...) {
         views.forEach { addSubview($0) }
     }
+    
+    func pinToEdges(of superview: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor)
+        ])
+    }
 }
--- GHFollowers/Screens/UserInfoViewController.swift
+++ GHFollowers/Screens/UserInfoViewController.swift
@@ -13,6 +13,9 @@ protocol UserInfoViewControllerDelegate: class {
 
 class UserInfoViewController: GFDataLoadingViewController {
     
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    
     let headerView = UIView()
     let itemViewOne = UIView()
     let itemViewTwo = UIView()
@@ -26,6 +29,7 @@ class UserInfoViewController: GFDataLoadingViewController {
         super.viewDidLoad()
 
         configureViewController()
+        configureScrollView()
         layoutUI()
         getUserInfo()
     }
@@ -63,6 +67,18 @@ class UserInfoViewController: GFDataLoadingViewController {
         self.dateLabel.text = "GitHub since \(user.createdAt?.convertToMonthYearFormat() ?? "")"
     }
     
+    private func configureScrollView() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        scrollView.pinToEdges(of: view)
+        contentView.pinToEdges(of: scrollView)
+                
+        NSLayoutConstraint.activate([
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.heightAnchor.constraint(equalToConstant: 600)
+        ])
+    }
+    
     // MARK: - UI
     
     private func layoutUI() {
@@ -71,16 +87,16 @@ class UserInfoViewController: GFDataLoadingViewController {
         itemViews = [headerView, itemViewOne, itemViewTwo, dateLabel]
         
         itemViews.forEach { itemView in
-            view.addSubview(itemView)
+            contentView.addSubview(itemView)
             itemView.translatesAutoresizingMaskIntoConstraints = false
             NSLayoutConstraint.activate([
-                itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-                itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+                itemView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+                itemView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding)
             ])
         }
         
         NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
             headerView.heightAnchor.constraint(equalToConstant: 210),
             
             itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
